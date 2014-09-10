fb5d272435b61d49fff9c7cdba28344fc2db3fb3
mezoni <andrew.mezoni@gmail.com>
2014-09-10 12:43:42 +0600

    0.0.2

--- lib/action_generator.dart
+++
@@ -1,5 +0,0 @@
-library string_matching.action_generator;
-
-import "package:string_matching/generators.dart";
-
-part "src/action_generator/action_generator.dart";
--- lib/interpreter.dart
+++
@@ -1,5 +0,0 @@
-library string_matching.interpreter;
-
-import "package:string_matching/instructions.dart";
-
-part "src/interpreter/interpreter.dart";
